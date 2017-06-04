6f0b0bc4893d1d9022f9dc7e29048fc5888edaf8
Boedaxbayah-vpn <indraapriana10@gmail.com>
2017-06-05 01:07:34 +0700

    Update user-renew.sh

--- user-renew.sh
+++ user-renew.sh
@@ -16,7 +16,7 @@ fi
 
 echo "----------------- TAMBAH MASA AKTIF AKUN SSH --------------------"
 
-	echo "        DEVELOPED BY YUSUF-ARDIANSYAH atau (082139743432)           "
+	echo "        DEVELOPED BY Boedaxbayah          "
 echo ""
 
 # begin of user-list
@@ -70,7 +70,7 @@ echo -e "   DI LARANG:                              "| lolcat
 echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        " | lolcat
 echo -e "   CARDING-CRIMINAL CYBER.                 "| lolcat
 echo -e "===========================================" | lolcat
-echo -e "   Script by YUSUF-ARDIANSYAH "       | lolcat
+echo -e "   Script by Boedaxbayah "       | lolcat
 echo -e "   Config OVPN:                            "| lolcat
 echo -e "   http://$MYIP:81/client.ovpn        " | lolcat
 echo -e "-------------------------------------------" | lolcat
